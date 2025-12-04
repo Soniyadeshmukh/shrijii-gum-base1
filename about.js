b3e8e249c630162e6cd9775ea20b8ac9a7273068
Soniyadeshmukh <soniyadeshmukh23@gmail.com>
2025-12-04 22:43:41 +0530

    Added about.html

---
+++ about.js
@@ -0,0 +1,12 @@
+let link = document.querySelector('#link');
+document.querySelector('#menu-btn').onclick = () =>{
+    link.classList.toggle('active');
+}
+let icons = document.querySelector('#icons');
+document.querySelector('#click-me').onclick = () =>{
+    icons.classList.toggle('active');
+}
+
+
+
+
---
+++ script.js
@@ -0,0 +1,13 @@
+let anchor = document.querySelector('#anchor');
+document.querySelector('#menu-btn').onclick = () =>{
+    anchor.classList.toggle('active');
+}
+let anchor1 = document.querySelector('#anchor1');
+document.querySelector('#click-me').onclick = () =>{
+    anchor1.classList.toggle('active');
+}
+
+
+
+
+
